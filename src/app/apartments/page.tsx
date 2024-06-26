efdb734fdb5689abd6ae6abd5fd8d641132835c2
Carlos Guevara <carlosguevara8959@outlook.com>
2024-06-26 14:42:44 -0400

    feat(apartments): improve details button placement in modal view (main)
    Adjusted the layout of the modal view in the apartments page.
    Now, the "Ver Detalles" link is centered, providing a better
    user experience and a more aesthetic layout.

--- src/app/apartments/page.tsx
+++ src/app/apartments/page.tsx
@@ -76,7 +76,9 @@ export default function Apartments() {
                         <p className="text-xs text-gray-500 mt-2">Price: ${selectedApartment.price}</p>
                         <div className="flex justify-between">
                             <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 py-2 px-4 mt-4 rounded-md" onClick={handleCloseModal}>Close</button>
-                            <Link href={`/apartment/${selectedApartment.id}`}>Ver Detalles</Link>
+                            <div className="flex items-center justify-center">
+                                <Link href={`/apartment/${selectedApartment.id}`}>Ver Detalles</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
